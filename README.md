fe9ce44cffd6ec62a30fc927157e51ed17e16a24
Kaylynn Tu <kaylynntu@mines.edu>
2019-07-06 14:45:10 -0600

    Added Source.cpp and continued to translate code into C++

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # CSMExoskeleton
 Colorado School of Mines Exoskeleton Code
+
+Authors:
+Date:
+
+This code is used to run the exoskeleten designed and built by the Colorado School of Mines Robotics Exoskeleton Team
--- Source.cpp
+++ Source.cpp
@@ -11,8 +11,11 @@ const int PotL = A0;
 const int PotR = A1;
 
 const int ROLLING_AVG_SIZE = 10;
-float rollingAvgListLeft[ROLLING_AVG_SIZE] = {};
+float rollingAvgListLeft[ROLLING_AVG_SIZE] = {};	
 float rollingAvgListRight[ROLLING_AVG_SIZE] = {};
+//TODO: (confirm) newest value is at the 0th index and oldest is at the largest index
+const int NEW_MEASUREMENT_INDEX = 0;
+const int OLDEST_MEASUREMENT_INDEX = ROLLING_AVG_SIZE;
 float rollingAvgLeft = 0;
 float rollingAvgRight = 0;
 
@@ -35,14 +38,55 @@ void setup() {
 	getRollingAvgs();
 }
 
-void translate(float val, int in_min, int in_max, int out_min, int out_max) {
+//Stops actuator
+void stop(actuator) {
+	actuator.forward(0);
+	//TODO: see if these commands work in C++
+	actuator.stop();
+}
+
+//Retracts actuator
+void retract(actuator) {
+	actuator.backward(1);
+	sleep(2);
+	stop(actuator);
+}
+
+//Moves actuator (expands)
+	//A negative speed value will move the actuator backwards
+void move(actuator, speed) {
+	
+	//TODO: what happens if speed == 0?, do we want it to move backwards?
+	//		Or do we need an error statement?
+	if (speed > 0) {
+		actuator.forward(speed);
+	} else {
+		actuator.backward(abs(speed));
+	}
+		
+}
+
+
+//Translates the raw input from the ADC into a 0-1 number
+float translate(float val, int inMin, int inMax, 
+				int outMin, int outMax) {
+	
+	int rangeIn = inMax - inMin;
+	int rangeOut = outMax - outMin;
+	
+	//Converts the value from ADC into a 0-1 range (float value)
+	float valueScaled = (float)(val - inMin) / (float)(rangeIn);
+	
+	//Shift the 0-1 number into the correct range
+	return outMin + (valueScaled * rangeOut);
 
 }
 
 //This collects all of the rolling averages for legs
 void getRollingAvgs() {
+	
 	for (int i = 0; i < ROLLING_AVG_SIZE; i++) {
-		int left = translate(analogRead(PotL), 0, maxReading;
+		int left = translate(analogRead(PotL), IN_MIN, maxReading, OUT_MIN, OUT_MAX);
 		int right = analogRead(PotR);
 
 		rollingAvgListLeft[i] = left;
@@ -59,7 +103,28 @@ void getRollingAvgs() {
 	rollingAvgRight /= ROLLING_AVG_SIZE;
 }
 
+//Here is where we tell things to move
+void moveLegs(actuator, chan, maxReading, deadZone, scaleFactor, &rollingAvgList) {
+	//Declare Vars
+	int IN_MIN = 0;
+	int OUT_MIN = -1;
+	int OUT_MAX = 1;
+	
+	//Insert new reading and delete oldest reading
+	rollingAvgList[NEW_MEASUREMENT_INDEX] = translate(chan.voltage, IN_MIN, maxReading, OUT_MIN, OUT_MAX);
+	//TODO: change rollingAvgLists to be a queue
+	
+	//If the average is larger than the dead zone, then move it
+	if (abs(rollingAvg <= deadZone)) {
+		stop(actuator);
+	} else {
+		//Do stuff
+	}
+}
 
+
+//Void loop acts as main
 void loop() {
-
+	moveLeg(/*left actuator, some stuff*/);
+	moveLeg(/*right actuator, some stuff*/);
 }
