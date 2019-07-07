d0115ea4f0a8f2883bdf72a927d26bb849eccd84
Ryan Reschak <ryanjreschak@mymail.mines.edu>
2019-07-06 22:14:11 -0600

    I primarily added a class for the actuator that is going to be given everything for on actuator to work

--- Source.cpp
+++ Source.cpp
@@ -11,13 +11,126 @@ const int PotL = A0;
 const int PotR = A1;
 
 const int ROLLING_AVG_SIZE = 10;
-float rollingAvgListLeft[ROLLING_AVG_SIZE] = {};	
-float rollingAvgListRight[ROLLING_AVG_SIZE] = {};
+
 //TODO: (confirm) newest value is at the 0th index and oldest is at the largest index
 const int NEW_MEASUREMENT_INDEX = 0;
 const int OLDEST_MEASUREMENT_INDEX = ROLLING_AVG_SIZE;
-float rollingAvgLeft = 0;
-float rollingAvgRight = 0;
+
+//Declare Vars
+const int IN_MIN = 0;
+const int OUT_MIN = -1;
+const int OUT_MAX = 1;
+
+class Actuator {
+public:
+	Actuator(int pinAct, int pinPot);
+
+	//TODO: WE NEED TO ADD A FUNCTION THE MOVES THE MOTOR BACK AND FORWARD (DECLARED BUT NOT DEFINED)
+	void forward();
+	void backward();
+	void stop();
+	void retract();
+	void move(speed);
+
+	void moveLeg(int chan, float maxReading, float deadZone, float scaleFactor);
+	float translate(float val, int inMin, int inMax,
+		int outMin, int outMax);
+
+	int getPin() const { return _pinNum; }
+	int getRollingAvgList() const { return _rollingAvgList; }
+	int getRollingAvg() const { return _rollingAvg; }
+private:
+	//pin for the actuator
+	int _pinActNum;
+	//pin for the potentiometer
+	int _pinPotNum;
+
+	float _rollingAvgList[ROLLING_AVG_SIZE] = {};
+	float _rollingAvg = 0;
+};
+
+//This collects all of the rolling averages for legs and overall intitalizes the leg
+Actuator::Actuator(int pinAct, int pinPot) {
+	//this assigns the pin number for the actuator
+	this->_pinNum = pinAct;
+
+	//this obtains the 
+	for (int i = 0; i < ROLLING_AVG_SIZE; i++) {
+		int val = translate(analogRead(pinPot), IN_MIN, maxReading, OUT_MIN, OUT_MAX);
+
+		_rollingAvgList[i] = val;
+	}
+
+	//sums
+	for (int i = 0; i < ROLLING_AVG_SIZE; i++) {
+		_rollingAvg += _rollingAvgList[i];
+	}
+
+	_rollingAvg /= ROLLING_AVG_SIZE;
+}
+
+//Stops actuator
+void Actuator::stop() {
+	forward(0);
+	//TODO: see if these commands work in C++
+	actuator.stop();
+}
+
+//Retracts actuator
+void Actuator::retract() {
+	backward(1);
+	sleep(2);
+	stop(actuator);
+}
+
+//Moves actuator (expands)
+//A negative speed value will move the actuator backwards
+void Actuator::move(speed) {
+
+	//TODO: what happens if speed == 0?, do we want it to move backwards?
+	//		Or do we need an error statement?
+	if (speed > 0) {
+		actuator.forward(speed);
+	}
+	else {
+		actuator.backward(abs(speed));
+	}
+
+}
+
+
+//Translates the raw input from the ADC into a 0-1 number
+float Actuator::translate(float val, int inMin, int inMax,
+	int outMin, int outMax) {
+
+	int rangeIn = inMax - inMin;
+	int rangeOut = outMax - outMin;
+
+	//Converts the value from ADC into a 0-1 range (float value)
+	float valueScaled = (float)(val - inMin) / (float)(rangeIn);
+
+	//Shift the 0-1 number into the correct range
+	return outMin + (valueScaled * rangeOut);
+
+}
+
+
+
+//Here is where we tell things to move
+void Actuator::moveLeg(int chan, float maxReading, float deadZone, float scaleFactor) {
+
+	//Insert new reading and delete oldest reading
+	rollingAvgList[NEW_MEASUREMENT_INDEX] = translate(chan.voltage, IN_MIN, maxReading, OUT_MIN, OUT_MAX);
+	//TODO: change rollingAvgLists to be a queue
+
+	//If the average is larger than the dead zone, then move it
+	if (abs(rollingAvg <= deadZone)) {
+		stop(actuator);
+	}
+	else {
+		//Do stuff
+	}
+}
 
 
 void setup() {
@@ -38,93 +151,10 @@ void setup() {
 	getRollingAvgs();
 }
 
-//Stops actuator
-void stop(actuator) {
-	actuator.forward(0);
-	//TODO: see if these commands work in C++
-	actuator.stop();
-}
-
-//Retracts actuator
-void retract(actuator) {
-	actuator.backward(1);
-	sleep(2);
-	stop(actuator);
-}
-
-//Moves actuator (expands)
-	//A negative speed value will move the actuator backwards
-void move(actuator, speed) {
-	
-	//TODO: what happens if speed == 0?, do we want it to move backwards?
-	//		Or do we need an error statement?
-	if (speed > 0) {
-		actuator.forward(speed);
-	} else {
-		actuator.backward(abs(speed));
-	}
-		
-}
-
-
-//Translates the raw input from the ADC into a 0-1 number
-float translate(float val, int inMin, int inMax, 
-				int outMin, int outMax) {
-	
-	int rangeIn = inMax - inMin;
-	int rangeOut = outMax - outMin;
-	
-	//Converts the value from ADC into a 0-1 range (float value)
-	float valueScaled = (float)(val - inMin) / (float)(rangeIn);
-	
-	//Shift the 0-1 number into the correct range
-	return outMin + (valueScaled * rangeOut);
-
-}
-
-//This collects all of the rolling averages for legs
-void getRollingAvgs() {
-	
-	for (int i = 0; i < ROLLING_AVG_SIZE; i++) {
-		int left = translate(analogRead(PotL), IN_MIN, maxReading, OUT_MIN, OUT_MAX);
-		int right = analogRead(PotR);
-
-		rollingAvgListLeft[i] = left;
-		rollingAvgListRight[i] = right;
-	}
-
-	//sums
-	for (int i = 0; i < ROLLING_AVG_SIZE; i++) {
-		rollingAvgLeft += rollingAvgListLeft[i];
-		rollingAvgRight += rollingAvgListRight[i];
-	}
-
-	rollingAvgLeft /= ROLLING_AVG_SIZE;
-	rollingAvgRight /= ROLLING_AVG_SIZE;
-}
-
-//Here is where we tell things to move
-void moveLegs(actuator, chan, maxReading, deadZone, scaleFactor, &rollingAvgList) {
-	//Declare Vars
-	int IN_MIN = 0;
-	int OUT_MIN = -1;
-	int OUT_MAX = 1;
-	
-	//Insert new reading and delete oldest reading
-	rollingAvgList[NEW_MEASUREMENT_INDEX] = translate(chan.voltage, IN_MIN, maxReading, OUT_MIN, OUT_MAX);
-	//TODO: change rollingAvgLists to be a queue
-	
-	//If the average is larger than the dead zone, then move it
-	if (abs(rollingAvg <= deadZone)) {
-		stop(actuator);
-	} else {
-		//Do stuff
-	}
-}
-
 
 //Void loop acts as main
 void loop() {
+
 	moveLeg(/*left actuator, some stuff*/);
 	moveLeg(/*right actuator, some stuff*/);
 }
