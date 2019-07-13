c5cac4961cffcda64d5f967b3a7a5dba82413567
Ryan Reschak <ryanjreschak@mymail.mines.edu>
2019-07-13 11:47:06 -0600

    For the most part finished. It needs the Particle Functions and exact values for the deadzone and scale factor for each leg

--- Source.cpp
+++ Source.cpp
@@ -12,10 +12,6 @@ const int PotR = A1;
 
 const int ROLLING_AVG_SIZE = 10;
 
-//TODO: (confirm) newest value is at the 0th index and oldest is at the largest index
-const int NEW_MEASUREMENT_INDEX = 0;
-const int OLDEST_MEASUREMENT_INDEX = ROLLING_AVG_SIZE;
-
 //Declare Vars
 const int IN_MIN = 0;
 const int OUT_MIN = -1;
@@ -33,7 +29,7 @@ public:
 	void move(int speed);
 
 	//These are the "main" functions that run all necessary bits 
-	void moveLeg(float maxReading, float deadZone, float scaleFactor);
+	void moveLeg(float deadZone, float scaleFactor);
 	float translate(float val, int inMin, int inMax,
 		int outMin, int outMax);
 
@@ -53,6 +49,7 @@ private:
 
 	float _rollingAvgList[ROLLING_AVG_SIZE] = {};
 	float _rollingAvg = 0;
+	float _maxReading = 0
 };
 
 //This collects all of the rolling averages for legs and overall intitalizes the leg
@@ -62,12 +59,9 @@ Actuator::Actuator(int pinAct, int pinPot, int pinDir) {
 	this->_pinPotNum = pinPot;
 	this->_pinDirNum = pinDir;
 
-	//this obtains the 
+	//this obtains the voltage values
 	for (int i = 0; i < ROLLING_AVG_SIZE; i++) {
-		//TODO: FIND OUT MAXREADING VALUE HERE 
-		int val = translate(analogRead(_pinPotNum), IN_MIN, maxReading, OUT_MIN, OUT_MAX);
-
-		_rollingAvgList[i] = val;
+		_rollingAvgList[i] = analogRead(_pinPotNum);
 	}
 
 	//sums
@@ -76,8 +70,10 @@ Actuator::Actuator(int pinAct, int pinPot, int pinDir) {
 	}
 
 	_rollingAvg /= ROLLING_AVG_SIZE;
+	_maxReading = 2 * _rollingAvg
 }
 
+//TODO: WHILE TESTING CONFIRM THAT HIGH AND LOW IS THE RIGHT DIRECTIONS IF NOT SWITCH THEM
 void Actuator::forward(int speed) {
 	analogWrite(_pinActNum, speed);
 	digitalWrite(_pinDirNum, HIGH);
@@ -108,10 +104,10 @@ void Actuator::move(int speed) {
 		forward(speed);
 	}
 	else if (speed < 0){
-		backward(abs(speed));
+		backward(-1*speed);
 	}
 	else {
-		stop()
+		stop();
 	}
 
 }
@@ -135,18 +131,30 @@ float Actuator::translate(float val, int inMin, int inMax,
 
 
 //Here is where we tell things to move
-void Actuator::moveLeg(float maxReading, float deadZone, float scaleFactor) {
+void Actuator::moveLeg(float deadZone, float scaleFactor) {
 
 	//Insert new reading and delete oldest reading
-	rollingAvgList[NEW_MEASUREMENT_INDEX] = translate(analogRead(_pinPotNum), IN_MIN, maxReading, OUT_MIN, OUT_MAX);
-	//TODO: change rollingAvgLists to be a queue
+	int temp = _rollingAvgList[0];
+	_rollingAvgList[0] = translate(analogRead(_pinPotNum), IN_MIN, _maxReading, OUT_MIN, OUT_MAX);
+	//Rolls over all the values one place
+	for (int i = 1; i < ROLLING_AVG_SIZE-1; i++) {
+		int temp2 = _rollingAvgList[i];
+		_rollingAvgList[i] = temp;
+		temp = temp2;
+	}
+	//gets the average again
+	for (int i = 0; i < ROLLING_AVG_SIZE; i++) {
+		_rollingAvg += _rollingAvgList[i];
+	}
 
+	_rollingAvg /= ROLLING_AVG_SIZE;
+	
 	//If the average is larger than the dead zone, then move it
 	if (abs(_rollingAvg <= deadZone)) {
 		stop();
 	}
 	else {
-		move(_rollingAvg)
+		move(_rollingAvg*scaleFactor);
 	}
 }
 
@@ -175,7 +183,7 @@ void setup() {
 
 //Void loop acts as main
 void loop() {
-	
+	//TODO: FIND OUT THE DEAD ZONE AND SCALE FACTOR FOR EACH LEG
 	actuatorL.moveLeg(/*some stuff*/);
 	actuatorR.moveLeg(/*some stuff*/);
 	
